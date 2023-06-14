3f978d5651f34b01fb5bf95dbc46fd91fe29ee7d
sALTanat5 <126831834+sALTanat5@users.noreply.github.com>
2023-06-14 11:41:48 -0500

    Update with variables main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-region = "us-east-2"
+region = var.region
 }
 
 
 resource "aws_instance" "web" {
-ami = "ami-092b51d9008adea15"
-instance_type = "t2.micro"
-availability_zone = "us-east-2a"
-key_name = "jenkins-terr-gcp-key"
+ami = var.ami_id
+instance_type = var.instance_type
+availability_zone = var.az1
+key_name = var.key_pair
 }
