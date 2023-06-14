6d038c25aa6d897f6a51ae53f6d6df778dfdf086
sALTanat5 <126831834+sALTanat5@users.noreply.github.com>
2023-06-14 11:42:56 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable ami_id {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable instance_type {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+
+variable az1 {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+
+variable key_pair {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable region {
+  type        = string
+  default     = ""
+  description = "description"
+}
