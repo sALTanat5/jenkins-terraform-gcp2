3cc4a8c46d9e2d150960dc6bb7f76f533a5e3ffb
sALTanat5 <126831834+sALTanat5@users.noreply.github.com>
2023-06-14 11:51:07 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,26 @@
 variable ami_id {
   type        = string
-  default     = ""
+  default     = "ami-092b51d9008adea15"
   description = "description"
 }
 
 variable instance_type {
   type        = string
-  default     = ""
+  default     = "t2.micro"
   description = "description"
 }
 
 
 variable az1 {
   type        = string
-  default     = ""
+  default     = "us-east-2a"
   description = "description"
 }
 
 
 variable key_pair {
   type        = string
-  default     = ""
+  default     = "jenkins-terr-gcp-key"
   description = "description"
 }
 
