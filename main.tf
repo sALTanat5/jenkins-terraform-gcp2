af34839e95e680076fc1aa336f19587ebf8b3d60
sALTanat5 <126831834+sALTanat5@users.noreply.github.com>
2023-06-15 19:45:45 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = var.region
 
 
 resource "aws_instance" "web" {
-ami = var.ami_id
+ami = data.aws_ami.ubuntu.id
 instance_type = var.instance_type
 availability_zone = var.az1
 key_name = var.key_pair
